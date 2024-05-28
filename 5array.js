03d06f890ba0ac92b915af2a1361d56ffd11d324
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-05-28 20:02:16 +0530

    add all chnages

---
+++ 5array.js
@@ -0,0 +1,14 @@
+//Array is the collection of homogenious or hetrigenious data type.
+
+var a = [1,2,3,44,55](Array of number)
+var b = ['sdf','tes','dfte','dfgr'](Array of String)
+var c = [true, false, false, true](Array of Boolean)
+
+var d = [3,5,6,7,'fdfgdf','feff',34,true, false,'sadds']
+
+var city = ["London","Delhi","NewYork"]
+undefined
+city.length
+3
+city[city.length-3]
+'London'
