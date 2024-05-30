b35761ed3853b21134f5fbedba5c6dee23a05947
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-05-30 12:36:28 +0530

    add all chnages

---
+++ 4switch.js
@@ -0,0 +1,29 @@
+// Multiple condition and multiple output(ifelse)
+// One condition and one output(ternary)
+// One condition and multiple output(switch)
+
+var name = "Santanu"
+
+switch(name){
+    case 'Santanu':
+        console.log(`Hi ${name}`)
+        break;
+        case 'Sidhanta':
+        console.log(`Hi ${Eva}`)
+        break;
+        default:
+                console.log(`hi default case`)
+}
+
+var input = 2
+
+switch(input%2){
+    case 0:
+        console.log(`Number is even`)
+        break;
+        case 1:
+        console.log(`Number is odd`)
+        break;
+        default:
+                console.log(`hi default case`)
+}
